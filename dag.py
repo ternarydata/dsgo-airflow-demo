4a1f78f2182d13c9a6ead43dd8b0f16838bfc5a8
mhousley <matt@ternarydata.com>
2021-07-25 08:11:38 -0600

    Improve documentation

--- dag.py
+++ dag.py
@@ -39,25 +39,26 @@ with airflow.DAG(
         python_callable=download_file,
     )
 
-    # Create BigQuery output dataset.
+    # Load the raw data into BigQuery.
     load_data_to_bq = BashOperator(
         task_id='load_data_to_bq',
         # Executing 'bq' command requires Google Cloud SDK which comes
         # preinstalled in Cloud Composer.
         bash_command='''bq load --autodetect \\
         --replace mobility_data.raw_data \\
-        gs://us-west2-airflow-dsgo-749fab29-bucket/data/raw-mobility-data.csv''')
+        gs://composer-data-bucket-path/raw-mobility-data.csv''')
     # Adjust this gcs path by finding your Composer GCS bucket and looking for
     # the data folder.
+    # The table name will work so long as you've used the dataset name 'mobility_data'.
 
     unpivot = BashOperator(
         task_id='unpivot',
         # Unpivot the data and create a table with the results.
-        # Adjust table names.
+        # Adjust the project name in both table names.
         bash_command='''bq query --nouse_legacy_sql \
-        'CREATE OR REPLACE TABLE `dsgo-demo-2021.mobility_data.unpivoted_data` as (
+        'CREATE OR REPLACE TABLE `your-project-name.mobility_data.unpivoted_data` as (
                 SELECT a.geo_type, region, transportation_type, unpivotted.*
-                FROM `dsgo-demo-2021.mobility_data.raw_data` a
+                FROM `your-project-name.mobility_data.raw_data` a
                 , UNNEST(fhoffa.x.cast_kv_array_to_date_float(fhoffa.x.unpivot(a, "_202"), "_%Y_%m_%d")) unpivotted
                 )'
         ''')
@@ -65,10 +66,12 @@ with airflow.DAG(
     export = BashOperator(
         task_id='export',
         # Export the processed data.
-        # Adjust table names so this can run.
+        # Adjust the bucket name so this will run.
+        # The table name should work so long as you've used the dataset name 'mobility_data'.
         bash_command='''bq extract 'mobility_data.unpivoted_data' \
-                        gs://dsgo-de-export-bucket/apple-mobility-export.csv
+                        gs://export-bucket-name/apple-mobility-export.csv
             ''')
 
+    # Set task dependencies.
     download>>load_data_to_bq>>unpivot>>export
 
